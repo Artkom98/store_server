f52693511cd2aac11a81d4a2e84616d04ac412e4
Artur Komyshniuk <akomisnuk@gmail.com>
2023-01-09 17:47:59 +0200

    added a readme file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Store server
+
+### Project install
+ - create a new directory `mkdir 'name'`
+ - go to this directory `cd 'name'`
+ - `git clone` https://github.com/Artkom98/store_server.git# clone*
+
+### Project launch
+
+- `cd store_server`
+- `python manage.py runserver`
+- Now that the server’s running, visit http://127.0.0.1:8000/
+
+### Stop the server
+
+- Stop the server using the combination `Ctrl + c`
--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from products.models import ProductCategory, Product, Basket
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+
 admin.site.register(Basket)
+
+
+@admin.register(Product)
+class AdminProducts(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity', 'category']
+    fields = ['name', 'image', 'description', 'short_descriptions', ('price', 'quantity'), 'category']
+    readonly_fields = ['short_descriptions']
+    ordering = ['-price']
+    search_fields = ['name']
+
+
+class BasketAdminInline(admin.TabularInline):
+    model = Basket
+    fields = ['product', 'quantity', 'created_timestamp']
+    readonly_fields = ['product', 'created_timestamp']
+    extra = 0
